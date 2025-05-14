8e889b312db372f75276f7d4eabf019ac0672772
FelipeAvila <becario2.sis@grupotarahumara.com.mx>
2025-05-14 15:56:58 -0600

    m

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       </div>
 
       <section className="px-4 md:px-10 lg:px-24 py-1 bg-gray-100">
-        <BookingCalendar />
+        <Gallery />
       </section>
  <Servicios />
       <section className="px-4 md:px-10 lg:px-24 py-1 bg-gray-100">
